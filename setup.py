1ffa4252a1c2b2370ce09d76e7eb217f2ce8269e
Radium2000 <rudrakalra20@gmail.com>
2025-08-21 16:18:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="plate-viz",
+    name="plateviz",
     version="0.1.0",
     description="Plate reader data visualization",
     author="Radium2000",
